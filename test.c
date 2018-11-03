bcaf7fd9eb5d919530a97987b2d2462685d6965d
fhanisch <felix.hanisch@live.de>
2018-11-04 00:30:28 +0100

    Direktvergleich

--- test.c
+++ test.c
@@ -18,25 +18,37 @@ void mul4x4(float*, float*, float*);
 
 float v1[] = { 1.0f, 2.0f, 3.0f, 4.0f };
 float v2[] = { 10.0f, 10.0f, -10.0f, -10.0f };
-mat4 A = { 1.5f, 2.5f, 3.5f, 4.5f,
-				5.5f, 6.5f, 7.5f, 8.5f,
-				-0.5f, -1.5f, -2.5f, -3.5f,
-				7.5f, -1.5f, 12.5f, -3.5f };
-mat4 B = { 1.5f, 2.5f, 3.5f, 4.5f,
-				5.5f, 6.5f, 7.5f, 8.5f,
-				-0.5f, -1.5f, -2.5f, -3.5f,
-				7.5f, -1.5f, 12.5f, -3.5f };
+float v3[] = { 1.0f, 2.0f, 3.0f, 4.0f };
+
+mat4 A = {	1.5f, 2.5f, 3.5f, 4.5f,
+			5.5f, 6.5f, 7.5f, 8.5f,
+			-0.5f, -1.5f, -2.5f, -3.5f,
+			7.5f, -1.5f, 12.5f, -3.5f };
+mat4 B = {	1.5f, 2.5f, 3.5f, 4.5f,
+			5.5f, 6.5f, 7.5f, 8.5f,
+			-0.5f, -1.5f, -2.5f, -3.5f,
+			7.5f, -1.5f, 12.5f, -3.5f };
+mat4 C;
 float P[] = {	1.5f, 2.5f, 3.5f, 4.5f,
 				5.5f, 6.5f, 7.5f, 8.5f,
 				-0.5f, -1.5f, -2.5f, -3.5f,
 				7.5f, -1.5f, 12.5f, -3.5f };
-float Q[] = { 1.5f, 2.5f, 3.5f, 4.5f,
+float Q[] = {	1.5f, 2.5f, 3.5f, 4.5f,
 				5.5f, 6.5f, 7.5f, 8.5f,
 				-0.5f, -1.5f, -2.5f, -3.5f,
 				7.5f, -1.5f, 12.5f, -3.5f };
 float R[16];
-float v3[] = { 1.0f, 2.0f, 3.0f, 4.0f };
-mat4 A, B, C;
+
+void printMat4x4(char *name, float *M)
+{
+	printf("%s =\n", name);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf(" %6.1f", M[j * 4 + i]);
+		}
+		printf("\n");
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -59,33 +71,37 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < 4; i++) printf(" %0.1f", v1[i]);
 	printf(" }\n");
 
-	//printMatrix4(A, "A");
-	printf("P:\n");
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			printf(" %6.1f", P[j * 4 + i]);
-		}
-		printf("\n");
-	}
-
+	//------------------------------------------------------------------
+	printf("**************************************************\n");
+	printf("Matrix multiplication using c compiler:\n");
+	printf("C = A * B\n");
+	printMatrix4(A, "A");
 	QueryPerformanceFrequency(&frequency);
 	QueryPerformanceCounter(&t1);
 	for (int i = 0; i < 1000; i++) {
-		//mult4(C, A, B);
+		mult4(C, A, B);
+	}
+	QueryPerformanceCounter(&t2);
+	elapsedTime.QuadPart = t2.QuadPart - t1.QuadPart;
+	elapsedTime.QuadPart *= 1000000;
+	elapsedTime.QuadPart /= frequency.QuadPart;
+	printMatrix4(C, "C");
+	printf("Calculated in %lld micro seconds.\n", elapsedTime.QuadPart);
+	//------------------------------------------------------------------
+	printf("**************************************************\n");
+	printf("Matrix multiplication using asm & sse:\n");
+	printf("R = P * Q\n");
+	printMat4x4("P", P);
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&t1);
+	for (int i = 0; i < 1000; i++) {
 		mul4x4(P, Q, R);
 	}
 	QueryPerformanceCounter(&t2);
 	elapsedTime.QuadPart = t2.QuadPart - t1.QuadPart;
 	elapsedTime.QuadPart *= 1000000;
 	elapsedTime.QuadPart /= frequency.QuadPart;
-	//printMatrix4(C, "C");
-	printf("R:\n");
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			printf(" %6.1f", R[j * 4 + i]);
-		}
-		printf("\n");
-	}
+	printMat4x4("R", R);
 	printf("Calculated in %lld micro seconds.\n", elapsedTime.QuadPart);
 	return 0;
 }
